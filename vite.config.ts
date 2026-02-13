3cb9836f740b8188a118baf61893d42da5387ca4
Deepthy Nair <deepthy.nair@2mnordic.com>
2026-02-13 10:55:03 +0100

    chore: setup github pages for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/emi-teacher-portal/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
